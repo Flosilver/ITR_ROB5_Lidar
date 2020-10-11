49a6619d754c966edaa21895cccdf8756512e4f1
Flosilver <florentin.b98@gmail.com>
2020-10-11 17:56:29 +0200

    ajout de dc_motor.h/.cpp

---
+++ include/dc_motor.h
@@ -0,0 +1,13 @@
+#ifndef DCMOTOR_H
+#define DCMOTOR_H
+
+class DCMotor
+{
+    public:
+        DCMotor();
+        ~DCMotor();
+
+    private:
+        int pin_;
+        
+}
---
+++ src/dc_motor.cpp
@@ -0,0 +1,12 @@
+#include "dc_motor.h"
+
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <unistd.h>
+
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+
+using namespace std;
