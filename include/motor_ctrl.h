fdfd51477a1ea8da2fcfd8fa1a060463dd3a6ef3
Florian Cormée <florian.cormee@gmail.com>
2020-12-05 17:26:02 +0100

    Implementation of the PI controller.

--- include/motor_ctrl.h
+++ include/motor_ctrl.h
@@ -1,33 +1,70 @@
 #ifndef __MOTOR_CTRL_H__
 #define __MOTOR_CTRL_H__
 
-#include "motor_dc.h"
+#include "dc_motor.h"
 #include "shaft_encoder.h"
 
 #include <iostream>
-#include <thread>
 #include <memory>
 #include <mutex>
 #include <string>
+#include <thread>
 
-class MotorCtrl : public DCMotor {
-    public:
-        MotorCtrl(int pin, int i2c_addr, std::string encoder_path, long encoder_cpr, float kp = 1, float ki = 1);
-        virtual ~MotorCtrl();
+class MotorCtrl : public DCMotor
+{
+    std::shared_ptr<ShaftEncoder> encoder_; ///< The shaft orientation sensor.
+    std::thread ctrl_thread_; ///< The thread with the control loop.
+    std::mutex ctrl_mtx_; ///< The mutex used by the control thread.
+    bool is_controlled_; ///< Whether the control loop should keep running or not.
+    const long sp_; ///< The sampling period (in microseconds).
+    float desired_orientation_; ///< The desired shat orientation (in radians).
+    const float kp_; ///< The proportional gain of the PI corrector.
+    const float ki_; ///< The integral gain of the PI corrector.
+    float err_int_; ///< The integrated error (in radians.seconds).
+    float anti_windup_; ///< The threshold at which to freeze the integral.
 
-        void rotate(float val);
+public:
+    /**
+     * Construct a new motor controller.
+     *
+     * @param pin The pin wired to the DC motor.
+     * @param i2c_addr The I2C address of the driver.
+     * @param encoder The encoder attached on the shaft.
+     * @param kp The proportional gain of the PI corrector.
+     * @param ki The integral gain of the PI corrector.
+     */
+    MotorCtrl(int pin, int i2c_addr, std::shared_ptr<ShaftEncoder> encoder, float kp = 1, float ki = 1);
 
-    private:
-        ShaftEncoder encoder_;
-        std::thread ctrl_thread_;
-        std::mutex input_mtx_;
-        float input_;
-        float kp_;
-        float ki_;
-        float err_int_;
-        bool bool_;
+    /**
+     * Destroy the motor controller.
+     *
+     * Stop the control loop and join the control thread. The motor is also
+     * stopped.
+     */
+    virtual ~MotorCtrl();
 
-        void control();
+    /**
+     * Rotate the shaft to the desired orientation.
+     *
+     * The rotation is performed asynchronously, thus the method returns
+     * instantly. There is no garanty of the position of the shaft when the
+     * method returns.
+     *
+     * NOTE: The orientation is relative to the encoder's home.
+     *
+     * @param orientation The disered orientation of the shaft (in radians).
+     */
+    void rotateAsync(float orientation);
+
+private:
+    /**
+     * Compute and send a velocity command to the motor.
+     *
+     * Takes the lock only to copy the disered orientation. Then computes a PI
+     * corrector and send the resulting command to the motor.
+     *
+     */
+    void control();
 };
 
 #endif
--- src/motor_ctrl.cpp
+++ src/motor_ctrl.cpp
@@ -1,7 +1,69 @@
 #include "motor_ctrl.h"
+#include <cassert>
+#include <chrono>
+#include <cmath>
 #include <functional>
 
-MotorCtrl::MotorCtrl(int pin, int i2c_addr, std::string encoder_path, long encoder_cpr, float kp, float ki): DCMotor(pin,i2c_addr), encoder_(encoder_path,encoder_cpr), kp_(kp), ki_(ki), input_(), err_int_()
+MotorCtrl::MotorCtrl(int pin, int i2c_addr, std::shared_ptr<ShaftEncoder> encoder, float kp = 1, float ki = 1) :
+    DCMotor(pin, i2c_addr),
+    encoder_(encoder),
+    kp_(kp),
+    ki_(ki),
+    desired_orientation_(0.0),
+    err_int_(0.0)
 {
-    ctrl_thread_ = std:thread(std::bind(MotorCtrl::control,this)); // oskour
+    assert(pin >= 0);
+    assert(i2c_addr >= 0);
+    assert(encoder != nullptr);
+    assert(kp >= 0.0);
+    assert(ki >= 0.0);
+    // Start the control thread
+    auto task = std::bind(MotorCtrl::control, this);
+    ctrl_thread_ = std::thread(task);
+}
+
+MotorCtrl::~MotorCtrl()
+{
+    // Stop the thread
+    {
+        std::lock_guard<std::mutex> ctrl_lock(ctrl_mtx_);
+        is_controlled_ = false;
+    }
+    ctrl_thread_.join();
+    // Stop the motor
+    motor_stop();
+}
+
+void MotorCtrl::rotateAsync(float orientation)
+{
+    std::lock_guard<std::mutex> ctrl_lock(ctrl_mtx_);
+    desired_orientation_ = orientation;
+}
+
+void MotorCtrl::control()
+{
+    float dt = sp_ * 1e-6;
+    while (is_controlled_)
+    {
+        // Copy the disered orientation
+        float des;
+        {
+            std::lock_guard<std::mutex> ctrl_lock(ctrl_mtx_);
+            des = desired_orientation_;
+        }
+        // Compute the PI corrector
+        float mes = encoder_->measurePosition();
+        float error = des - mes;
+        float float_command = kp_ * (error + ki_ * err_int_);
+        if (-anti_windup_ < float_command && float_command < anti_windup_)
+        {
+            err_int_ += error * dt;
+            float_command = kp_ * (error + ki_ * err_int_);
+        }
+        // Send the command to the motor
+        int int_command = (int)std::round(float_command);
+        run(int_command);
+
+        std::this_thread::sleep_for(std::chrono::microseconds(sp_));
+    }
 }
